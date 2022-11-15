26fe544ee2b7da726927e2d277fa4a97fcf29ccb
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-14 19:02:15 -0800

    python

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
-from app.models import Users
+from app.models import db, Users
+from app.config import Config
+
+def create_db():
+    db.create_all()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     return app
 
 def init_db():
-    
+    create_db()
+    admin = Users(name = "Pedro")
