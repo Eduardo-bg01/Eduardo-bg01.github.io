ddf45466c2b9f605455d9c8370a7d667e9127c5d
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-17 19:12:52 -0800

    Cambios para mostrar una pagina HTML/listas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from app import create_app, create_db
+from app import *
 from app.migrate import init_db
-
+from flask import render_template
 app = create_app()
 
 @app.route('/home')
@@ -11,3 +11,11 @@ def hello_world():
 def create_db():
     init_db()
     return "La base de datos ha sido creada"
+
+@app.route('/prueba')
+def create_db():
+    lista = ["valorant", "halo", "apex"]
+    return render_template("prueba.html", varx=lista)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
