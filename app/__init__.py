47c9cb0de4899a08b131eab75f3efe27aae056dc
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-21 16:10:42 -0800

    Ayuda

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from app.models import db
 from app.config import Config
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
     return app
     
--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
-from app import *
-from app.migrate import init_db
-from flask import render_template
+from app import create_app
+from flask import render_template, request, redirect
 app = create_app()
-
-@app.route('/home')
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/database')
-def create_db():
-    init_db()
-    return "La base de datos ha sido creada"
 
 @app.route('/prueba')
 def create_db():
     lista = ["valorant", "halo", "apex"]
     return render_template("prueba.html", varx=lista)
+
+@app.route('/formulario', methods=["GET", "POST"])
+def show_form():
+    if request.method == 'POST':
+        lista2 = [
+            request.form['input1'],
+            request.form['input2'],
+            request.form['input3']
+        ]
+        return render_template('data.html', lista2=lista2)
+    elif request.method == 'GET':
+        return render_template("formulario.html")
+    else:
+        return redirect('/prueba')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
