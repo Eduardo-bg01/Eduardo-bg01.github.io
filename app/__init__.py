6603bc34bee0337444cf8c608329127eb7279797
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-21 16:41:49 -0800

    Ayuda pt2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.config import Config
+from app.models import db
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    db.init_app(app)
     return app
-    
--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from app import create_app
-from flask import render_template, request, redirect
+from app.migrate import init_db
+from flask import render_template
+from flask import request, Flask
+
 app = create_app()
 
 @app.route('/prueba')
@@ -7,19 +10,26 @@ def create_db():
     lista = ["valorant", "halo", "apex"]
     return render_template("prueba.html", varx=lista)
 
-@app.route('/formulario', methods=["GET", "POST"])
-def show_form():
-    if request.method == 'POST':
-        lista2 = [
-            request.form['input1'],
-            request.form['input2'],
-            request.form['input3']
-        ]
-        return render_template('data.html', lista2=lista2)
-    elif request.method == 'GET':
-        return render_template("formulario.html")
-    else:
-        return redirect('/prueba')
+@app.route('/')
+def prueba():
+    return "<center><h1>Index</h1></center>"
+
+@app.route('/database')
+def create_db():
+    init_db()
+    return "La base de datos ha sido creada"
+
+@app.route('/formulario/')
+def form():
+    return render_template("formulario.html")
+
+@app.route("/retorno", methods=['GET'])
+def retorno():
+    nombreUser = request.args.get('nombreUser')
+    juego = request.args.get('juego')
+    longitud = len(juego)
+    
+    return "<center><h1>Bienvenido " + nombreUser + "</h1></center><br>""<center><h1> Juego favorito " + juego + "</h1></center><br>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
