f430da282d907f06a12ca9066ecc82157f3b2d83
Eduardo-bg01 <ebeltran8@uabc.edu.mx>
2022-11-22 19:31:00 -0800

    cambios

--- app/config.py
+++ app/config.py
@@ -6,4 +6,4 @@ class Config():
     MYSQL_DATABASE_HOST = "localhost"
     MYSQL_DATABASE_USER = "root"
     MYSQL_DATABASE_PASSWORD = "root"
-    MYSQL_DATABASE_DB = "clase3"
+    MYSQL_DATABASE_DB = "daw"
