524c520b512f37ac201aeb32d4b5414dac589f37
Eduardo Beltran Garcia <79712554+Eduardo-bg01@users.noreply.github.com>
2026-09-13 21:32:14 -0700

    Increase max RSVP party count from 4 to 5

--- js/rsvp.js
+++ js/rsvp.js
@@ -117,7 +117,7 @@ function updateRsvp(guest) {
     placeholder.disabled = true;
     placeholder.selected = true;
     rsvpPartyCount.appendChild(placeholder);
-    var max = Math.min(guest.party, 4);
+    var max = Math.min(guest.party, 5);
     for (var i = 1; i <= max; i++) {
       var opt = document.createElement('option');
       opt.value = i;
